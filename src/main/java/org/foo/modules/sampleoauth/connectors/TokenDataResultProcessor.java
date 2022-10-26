fc7163d5632e3abafc5fee2195c8900cbb282375
Thomas LE CLERE <tleclere@jahia.com>
2022-10-26 07:02:10 +0200

    Add login in token data connector result processor and cache results

--- src/main/java/org/foo/modules/sampleoauth/connectors/TokenDataResultProcessor.java
+++ src/main/java/org/foo/modules/sampleoauth/connectors/TokenDataResultProcessor.java
@@ -1,12 +1,17 @@
 package org.foo.modules.sampleoauth.connectors;
 
-import org.jahia.modules.jahiaauth.service.*;
+import org.jahia.modules.jahiaauth.service.ConnectorConfig;
+import org.jahia.modules.jahiaauth.service.ConnectorResultProcessor;
+import org.jahia.modules.jahiaauth.service.JahiaAuthConstants;
+import org.jahia.modules.jahiaauth.service.JahiaAuthMapperService;
+import org.jahia.modules.jahiaauth.service.MappedProperty;
+import org.jahia.modules.jahiaauth.service.MappedPropertyInfo;
 import org.jahia.modules.jahiaoauth.service.JahiaOAuthConstants;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.springframework.web.context.request.RequestContextHolder;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component(service = ConnectorResultProcessor.class, immediate = true)
@@ -23,8 +28,9 @@ public class TokenDataResultProcessor implements ConnectorResultProcessor {
     @Override
     public void execute(ConnectorConfig connectorConfig, Map<String, Object> results) {
         // store tokenData to cache
-        jahiaAuthMapperService.cacheMapperResults(MAPPER_NAME, RequestContextHolder.getRequestAttributes().getSessionId(),
-                Collections.singletonMap(JahiaOAuthConstants.TOKEN_DATA, new MappedProperty(
-                        new MappedPropertyInfo(JahiaOAuthConstants.TOKEN_DATA), results.get(JahiaOAuthConstants.TOKEN_DATA))));
+        Map<String, MappedProperty> mapperInfo = new HashMap<>();
+        mapperInfo.put(JahiaAuthConstants.SSO_LOGIN, new MappedProperty(new MappedPropertyInfo(JahiaAuthConstants.SSO_LOGIN), results.get("username")));
+        mapperInfo.put(JahiaOAuthConstants.TOKEN_DATA, new MappedProperty(new MappedPropertyInfo(JahiaOAuthConstants.TOKEN_DATA), results.get(JahiaOAuthConstants.TOKEN_DATA)));
+        jahiaAuthMapperService.cacheMapperResults(MAPPER_NAME, RequestContextHolder.getRequestAttributes().getSessionId(), mapperInfo);
     }
 }
